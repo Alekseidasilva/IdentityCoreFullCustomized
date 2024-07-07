96f9d7ed7e79d8626b99b1313492160971b72869
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-07-07 08:40:21 +0100

    FEAT: :sparkles: Part-15-Customizing Identity-> Refresh Token and Expires Fields to ApplicationUser

---
+++ IdentityCoreFullCustomized.Data/Migrations/20240707073137_AddAplicationUser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityCoreFullCustomized.Data.Migrations
+{
+    public partial class AddAplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0114e900-7edc-4efe-a09a-1c0123ceb6fc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c0d57bc6-7561-43d3-ab4e-720f437188c5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f91fe20d-0ec4-4186-a90d-3f09b8a1c42a");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiry",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4b74d8f5-1abd-4815-aa72-0fc0f22d0fa7", "2", "User", "User" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5a0264d0-ceba-4a56-bae0-825bba9328da", "3", "RH", "RH" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d34ea9ef-50e8-460c-a6d7-0e54421bb09b", "1", "Admin", "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4b74d8f5-1abd-4815-aa72-0fc0f22d0fa7");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5a0264d0-ceba-4a56-bae0-825bba9328da");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d34ea9ef-50e8-460c-a6d7-0e54421bb09b");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiry",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "0114e900-7edc-4efe-a09a-1c0123ceb6fc", "2", "User", "User" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c0d57bc6-7561-43d3-ab4e-720f437188c5", "3", "RH", "RH" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f91fe20d-0ec4-4186-a90d-3f09b8a1c42a", "1", "Admin", "Admin" });
+        }
+    }
+}
---
+++ IdentityCoreFullCustomized.Data/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityCoreFullCustomized.Data.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string RefreshToken { get; set; } = null!;
+    public DateTime RefreshTokenExpiry { get; set; }
+}
